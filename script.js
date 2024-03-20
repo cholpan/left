881dc4ab12a1ea0290e1883ec5f91a639b500771
cholpan <musaevacolpan28@gmail.com>
2024-03-20 18:13:34 +0600

    lift

---
+++ script.js
@@ -0,0 +1,82 @@
+let btn16 = document.querySelector(".btn16");
+let btn15 = document.querySelector(".btn15");
+let btn14 = document.querySelector(".btn14");
+let btn13 = document.querySelector(".btn13");
+let btn12 = document.querySelector(".btn12");
+let btn11 = document.querySelector(".btn11");
+let btn10 = document.querySelector(".btn10");
+let btn9 = document.querySelector(".btn9");
+let btn8 = document.querySelector(".btn8");
+let btn7 = document.querySelector(".btn7");
+let btn6 = document.querySelector(".btn6");
+let btn5 = document.querySelector(".btn5");
+let btn4 = document.querySelector(".btn4");
+let btn3 = document.querySelector(".btn3");
+let btn2 = document.querySelector(".btn2");
+let btn1 = document.querySelector(".btn1");
+
+let look = document.querySelector(".look img");
+
+btn16.addEventListener("click", () => {
+  look.style.transform = "translateY(-599px)";
+});
+
+btn15.addEventListener("click", () => {
+  look.style.transform = "translateY(-560px)";
+});
+btn14.addEventListener("click", () => {
+  look.style.transform = "translateY(-520px)";
+});
+
+btn13.addEventListener("click", () => {
+  look.style.transform = "translateY(-480px)";
+});
+
+btn12.addEventListener("click", () => {
+  look.style.transform = "translateY(-440px)";
+});
+
+btn11.addEventListener("click", () => {
+  look.style.transform = "translateY(-401px)";
+});
+
+btn10.addEventListener("click", () => {
+  look.style.transform = "translateY(-360px)";
+});
+
+btn9.addEventListener("click", () => {
+  look.style.transform = "translateY(-314px)";
+});
+
+btn8.addEventListener("click", () => {
+  look.style.transform = "translateY(-270px)";
+});
+
+btn7.addEventListener("click", () => {
+  look.style.transform = "translateY(-238px)";
+});
+
+btn6.addEventListener("click", () => {
+  look.style.transform = "translateY(-193px)";
+});
+
+btn5.addEventListener("click", () => {
+  look.style.transform = "translateY(-150px)";
+});
+
+btn4.addEventListener("click", () => {
+  look.style.transform = "translateY(-112px)";
+});
+
+btn3.addEventListener("click", () => {
+  look.style.transform = "translateY(-69px)";
+});
+
+btn2.addEventListener("click", () => {
+  look.style.transform = "translateY(-30px)";
+});
+
+btn1.addEventListener("click", () => {
+  look.style.transform = "translateY(0px)";
+});
+
